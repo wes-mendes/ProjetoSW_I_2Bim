de077edff74d4ba73b8100c5d7fccbd332dc2276
wes-mendes <wesleymendesdesousa7@gmail.com>
2024-06-20 08:13:16 -0300

    Primeiro envio Cadastro de Clientes

---
+++ CadastoClientes/Controllers/ClienteController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CadastroClientes.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Adicionar()
+        {
+            return View();
+        }
+        public IActionResult Editar()
+        {
+            return View();
+        }
+        public IActionResult Apagar()
+        {
+            return View();
+        }
+    }
+}
